5ce14d253481b5648e5f45763203c0b78577e1ee
mvarge <marcelo.varge@gmail.com>
2018-01-14 19:08:09 +0000

    first try

---
+++ files_finder.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from collections import deque
+
+class Finder(object):
+
+    def find(self, path):
+        for f in os.listdir(path):
+            p = os.path.join(path, f)
+            try:
+                if os.path.isfile(p):
+                    f = p if self.fullpath else f
+                    self.add(File(f, os.stat(p).st_size))
+                elif os.path.isdir(p):
+                    self.find(p)
+            except OSError:
+                # is this needed?
+                pass
+
+
+class Result(Finder):
+
+    def __init__(self, size, fullpath=False):
+        self.size = size
+        self.fullpath = fullpath
+        self.deque = deque(maxlen=size)
+        self.deque.append(File('', 0))
+
+    def add(self, file):
+        if file.size >= min([f.size for f in self.deque]):
+            self.sort()
+            self.deque.append(file)
+
+    def sort(self):
+        self.deque = deque(sorted(self.deque, key=lambda x: x.size), maxlen=self.size)
+
+
+class File(object):
+
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+
+
+if __name__ == "__main__":
+    r = Result(int(sys.argv[2]))
+    r.find(sys.argv[1])
+    for f in sorted(r.deque, key=lambda x: x.size, reverse=True):
+        print f.size, f.name
