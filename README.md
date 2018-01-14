1639b9d849fe6f310adb2892d5bcdceba25826fb
mvarge <marcelo.varge@gmail.com>
2018-01-14 21:05:25 +0000

    adding credits to sizeof function

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Program to recursively find (big) files under a tree/directory
 
 # About
+
+# Credits
+Sizeof function was taken from [this link](https://web.archive.org/web/20111010015624/http://blogmag.net/blog/read/38/Print_human_readable_file_size).
--- files_finder.py
+++ files_finder.py
@@ -42,10 +42,17 @@ class File(object):
         self.size = size
 
 
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','K','M','G','T','P','E','Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Y', suffix)
+
 if __name__ == "__main__":
     from helper import parse
     args = parse()
     r = Result(args.number, fullpath=args.fullpath)
     r.find(args.path)
     for f in sorted(r.deque, key=lambda x: x.size, reverse=True):
-        print f.size, f.name
+        print sizeof_fmt(f.size), f.name
